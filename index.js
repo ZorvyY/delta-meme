66219e7c6c5b1acaaf430b4c682eb8c5b89ec1e7
Zoravur Singh <zoravur.s@gmail.com>
2019-01-26 20:56:12 -0500

    got lit today

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+
+const msgHistory = [];
+
+const users = [];
+let userToReplace = 1;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket){
+  socket.emit('history', msgHistory);
   socket.on('chat message', function(msg){
+    msgHistory.push(msg);
     io.emit('chat message', msg);
   });
+
+  socket.emit('setUserNumber', userToReplace);
+  userToReplace = userToReplace === 1 ? 2 : 1;
+
+  users.push(socket);
+  if (users.length > 2) {
+    users[0].disconnect();
+    users.shift();
+  }
 });
 
 http.listen(port, function(){
